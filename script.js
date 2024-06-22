b22c0315594f2247e5e4c927d03afbccce4bde33
PIYUSH RAJ <121041810+Piyushraj2002@users.noreply.github.com>
2024-06-22 11:15:16 +0530

    Project2

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const clock = document.getElementById('clock');
+console.log("color switcher project");
+const buttons = document.querySelectorAll('.button');
+//console.log(buttons);
+const body = document.querySelector("body")
+//applying events : for color switching
+buttons.forEach(function(button){
+ //console.log(button);
+    button.addEventListener('click', function(e){
+                     //kya karega uske liye callback fn
+                console.log(e)
+                console.log(e.target)
+                if(e.target.id === 'grey'){
+                  body.style.backgroundColor = e.target.id;
+                }
 
-setInterval(function () {
-  let date = new Date();
-  clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
+                if(e.target.id === 'red'){
+                    body.style.backgroundColor = e.target.id;
+                  }
+
+                  if(e.target.id === 'blue'){
+                    body.style.backgroundColor = e.target.id;
+                  }
+
+                  if(e.target.id === 'yellow'){
+                    body.style.backgroundColor = e.target.id;
+                  }
+                
+    } );
+});
